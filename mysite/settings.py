16a92a73fe07cb383c2d6d2eb12c0fbe2928850e
Meow <you@example.com>
2026-03-28 11:58:47 -0400

    updated filename

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(os.path.join(BASE_DIR, 'prod.env'))
 
 SECRET_KEY = os.getenv('secret')
 
